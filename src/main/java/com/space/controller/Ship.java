5a09d7a757d9c800c2e6b2b96ec254a654da1f62
vvkarpov <chuzhoi36@gmail.com>
2021-01-01 21:08:38 +0300

    Add entity ship

---
+++ src/main/java/com/space/controller/Ship.java
@@ -0,0 +1,89 @@
+package com.space.controller;
+
+import com.space.model.ShipType;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Ship {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;//ID ship
+
+    private String name; //Ship name
+    private String planet;//Arrival planet
+    private ShipType shipType;//Ship type
+    private Date prodDate;//Release date
+    private Boolean isUsed;//Used/New
+    private Double speed;//Ship speed (
+    private Integer crewSize;//Crew size
+    private Double rating;//Ship rating
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPlanet() {
+        return planet;
+    }
+
+    public void setPlanet(String planet) {
+        this.planet = planet;
+    }
+
+    public ShipType getShipType() {
+        return shipType;
+    }
+
+    public void setShipType(ShipType shipType) {
+        this.shipType = shipType;
+    }
+
+    public Date getProdDate() {
+        return prodDate;
+    }
+
+    public void setProdDate(Date prodDate) {
+        this.prodDate = prodDate;
+    }
+
+    public Boolean getUsed() {
+        return isUsed;
+    }
+
+    public void setUsed(Boolean used) {
+        isUsed = used;
+    }
+
+    public Double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(Double speed) {
+        this.speed = speed;
+    }
+
+    public Integer getCrewSize() {
+        return crewSize;
+    }
+
+    public void setCrewSize(Integer crewSize) {
+        this.crewSize = crewSize;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+}
